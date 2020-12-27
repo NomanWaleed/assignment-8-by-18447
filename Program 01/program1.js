311ee9e27688111692194960bbfe20acf75856c4
nomanwaleed <m.noman23500@gmail.com>
2020-12-28 00:29:03 +0500

    Assignment 8 - V 1.0

--- Program 01/program1.js
+++
@@ -1,3 +0,0 @@
-var tempCelsius = parseInt(prompt("Enter Temperature in Celsius"));
-var tempFahren =  9/ (5 * tempCelsius) + 32;
-alert("Temperature in Fahrenheit is : " + tempFahren);
--- Program 02/program2.js
+++
@@ -1,3 +0,0 @@
-var heightInches = parseInt(prompt("Enter Height in Inches"));
-var heightCentiMeter = 2.54 * heightInches;
-console.log("Height in CentiMeters is : ", heightCentiMeter, "cm");
